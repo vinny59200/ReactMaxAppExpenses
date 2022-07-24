bbdb5343464631d2f739980839563023a9916518
DESKTOP-063BMPJ\Vinny <vvauban@gmail.com>
2022-07-24 16:50:25 +0200

    Data set

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import './App.css';
 const expenses = [
   {
     id: 'e1',
-    title: 'Toilet Paper',
+    title: 'Shirts',
     amount: 94.12,
-    date: new Date(2020, 7, 14),
+    date: new Date(2021, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2', title: 'Udemy courses', amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
+    title: 'Year transport fee',
+    amount: 512.50,
+    date: new Date(2022, 2, 28),
   },
   {
     id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
+    title: 'New Mic (Gamer)',
+    amount: 150,
+    date: new Date(2022, 5, 12),
   },
 ];
 function App() {
